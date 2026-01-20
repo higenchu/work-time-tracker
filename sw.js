f91841b6d11273ac0888b11bda2e1d58398257a1
YuuChibichichi <higenchu0628@gmail.com>
2026-01-21 02:19:16 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,6 @@
+// sw.js
+// PWAとして認識させるための最低限の記述
 self.addEventListener('fetch', function(event) {
-  // PWAとして認識させるための最低限の記述
+  // キャッシュせずにネットワークへリクエスト（更新を即反映させるため）
+  event.respondWith(fetch(event.request));
 });
